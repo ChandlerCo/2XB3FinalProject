6f0acb0b7ac71290622cf7525ef69395fd680bf1
root <root@Chanco.localdomain>
2019-04-11 21:40:56 -0400

    Added headings, format changes

--- JavaProject/src/collisionCourse/UserInterface.java
+++ JavaProject/src/collisionCourse/UserInterface.java
@@ -15,39 +15,42 @@ public class UserInterface implements ActionListener{
 	private JPanel thePanel;
 
 	// input fields
-	public Label Cmonth;
-	public Label Cday;
-	public Label Chour;
-	public Label Cwthr;
-	public Label Crsur;
+	private Label Cmonth;
+	private Label Cday;
+	private Label Chour;
+	private Label Cwthr;
+	private Label Crsur;
 	
-	public Label Vtype;
-	public Label Vyear;
+	private Label Vtype;
+	private Label Vyear;
 	
-	public Label Psex;
-	public Label Page;
+	private Label Psex;
+	private Label Page;
 	
-	public ComboBox cMonth;
-	public ComboBox cDay;
-	public JSpinner cHour;
-	public ComboBox cWthr;
-	public ComboBox cRsur;
+	private ComboBox cMonth;
+	private ComboBox cDay;
+	private JSpinner cHour;
+	private ComboBox cWthr;
+	private ComboBox cRsur;
 
-	public ComboBox vType;
-	public JSpinner vYear;
+	private ComboBox vType;
+	private JSpinner vYear;
 
-	public ComboBox pSex;
-	public JSpinner pAge;
+	private ComboBox pSex;
+	private JSpinner pAge;
 
-	public JButton go;
+	private JButton go;
 	
-	public JLabel total;
-	//public JLabel totSev;
+	private Label headings;
+	private Label headings2;
 	
-	public JLabel topPer[];
-	public JLabel topSev[];
+	private Label total;
+	private Label headPer;
+	private Label headSev;
+	private Label topPer[];
+	private Label topSev[];
 	
-	public Values output;
+	private Values output;
 	private static ArrayList<Entry> data;
 	// Constructor
 	public UserInterface(String strHeader, int intL, int intW) {
@@ -75,34 +78,34 @@ public class UserInterface implements ActionListener{
 		cHour.setLocation(330, 40);
 		thePanel.add(cHour);
 		// weather
-		Cwthr = new Label("Weather conditions", 150, 30, 495, 10, thePanel);
+		Cwthr = new Label("Weather conditions", 250, 30, 495, 10, thePanel);
 		String[] wthr = { "Select one", "Clear and sunny", "Overcast, cloudy but no precipitation", "Raining",
 				"Snowing", "Freezing rain, sleet, hail", "Visibility limitation", "Strong wind" };
-		cWthr = new ComboBox(wthr, 150, 30, 490, 40, 0, thePanel);
+		cWthr = new ComboBox(wthr, 250, 30, 490, 40, 0, thePanel);
 		// road surface
-		Crsur = new Label("Road surface conditions", 150, 30, 655, 10, thePanel);
+		Crsur = new Label("Road surface conditions", 150, 30, 755, 10, thePanel);
 		String[] rsur = { "Select one", "Dry, normal", "Wet", "Snow", "Slush, wet snow", "Icy", "Sandy/dirty", "Muddy",
 				"Oil", "Flooded" };
-		cRsur = new ComboBox(rsur, 150, 30, 650, 40, 0, thePanel);
+		cRsur = new ComboBox(rsur, 150, 30, 750, 40, 0, thePanel);
 		// vehicle type
-		Vtype = new Label("Vehicle type", 150, 30, 15, 70, thePanel);
+		Vtype = new Label("Vehicle type", 250, 30, 15, 70, thePanel);
 		String[] vtype = { "Select one", "Passenger car/van, light utility vehicle", "Cargo van", "Other trucks/vans",
 				"Unit trucks >4536 KG", "Road tractor", "School bus", "City Bus", "Motorcycle, moped",
 				"Off road vehicle", "Bicycle", "Motorhome", "Farm equipment", "Construction equipment", "Fire engine",
 				"Snowmobile", "Street car" };
-		vType = new ComboBox(vtype, 150, 30, 10, 100, 0, thePanel);
+		vType = new ComboBox(vtype, 250, 30, 10, 100, 0, thePanel);
 		// vehicle year
-		Vyear = new Label("Vehicle year(1900 for no answer)", 150, 30, 175, 70, thePanel);
+		Vyear = new Label("Vehicle year(1900 for no answer)", 250, 30, 275, 70, thePanel);
 		vYear = new JSpinner(new SpinnerNumberModel(2000, 1900, 2017, 1));
 		vYear.setSize(150, 30);
-		vYear.setLocation(170, 100);
+		vYear.setLocation(270, 100);
 		thePanel.add(vYear);
 		// sex
 		Psex = new Label("Sex", 150, 30, 15, 130, thePanel);
 		String[] sex = { "Prefer not to say", "Female", "Male" };
 		pSex = new ComboBox(sex, 150, 30, 10, 160, 0, thePanel);
 		// age
-		Page = new Label("Age (100 for prefer not to answer)", 150, 30, 175, 130, thePanel);
+		Page = new Label("Age (100 for prefer not to answer)", 250, 30, 175, 130, thePanel);
 		pAge = new JSpinner(new SpinnerNumberModel(25, 16, 100, 1));
 		pAge.setSize(150, 30);
 		pAge.setLocation(170, 160);
@@ -115,43 +118,20 @@ public class UserInterface implements ActionListener{
 		go.addActionListener(this);
 		thePanel.add(go);
 		
+		headings = new Label("% of all collisions   % of collisions involving factor which", 600, 25, 35, 200, thePanel);
+		headings2 = new Label("involving factor      resulted in serious injury or fatality", 600, 25, 35, 220, thePanel);
 		// total
-		total = new JLabel("Total Percent");
-		total.setSize(300,25);
-		total.setLocation(25, 200);
-		thePanel.add(total);
-		/*
-		totSev = new JLabel("Total severe");
-		totSev.setSize(100,25);
-		totSev.setLocation(150, 200);
-		thePanel.add(totSev);
-		*/
-		topPer = new JLabel[3];
-		//tpSev = new JLabel[3];
+		total = new Label("", 300, 25, 25, 250, thePanel);
+
+		headPer = new Label("Top 3 factors with highest occurence percentage", 300, 25, 25, 275, thePanel);
+		headPer = new Label("Top 3 factors with highest severity percentage", 300, 25, 25, 390, thePanel);
+		topPer = new Label[3];
 		
-		topSev = new JLabel[3];
+		topSev = new Label[3];
 		//tsSev = new JLabel[3];
 		for (int i = 0; i < 3; i++) {
-			topPer[i] = new JLabel("Top Percent");
-			topPer[i].setSize(300,25);
-			topPer[i].setLocation(25, 240 + i * 40);
-			thePanel.add(topPer[i]);
-			/*
-			tpSev[i] = new JLabel("Top Severe");
-			tpSev[i].setSize(100,25);
-			tpSev[i].setLocation(150, 240 + i * 40);
-			thePanel.add(tpSev[i]);
-			*/
-			topSev[i] = new JLabel("Top Percent");
-			topSev[i].setSize(300,25);
-			topSev[i].setLocation(25, 400 + i * 40);
-			thePanel.add(topSev[i]);
-			/*
-			tsSev[i] = new JLabel("Top Severe");
-			tsSev[i].setSize(100,25);
-			tsSev[i].setLocation(150, 400 + i * 40);
-			thePanel.add(tsSev[i]);
-			*/
+			topPer[i] = new Label("", 300, 25, 25, 300 + i * 30, thePanel);
+			topSev[i] = new Label("", 300, 25, 25, 415 + i * 30, thePanel);
 		}
 		
 		
@@ -162,7 +142,7 @@ public class UserInterface implements ActionListener{
 	
 	public static void main(String[] args) throws IOException {
 		data = ReadData.Read("data\\NCDB_2016.csv");
-		UserInterface ui = new UserInterface("Collision Course", 600, 810);
+		UserInterface ui = new UserInterface("Collision Course", 600, 910);
 	}
 
 	@Override
@@ -186,16 +166,14 @@ public class UserInterface implements ActionListener{
 			if (sex == 0) sex = -1;
 			int age = (Integer) pAge.getValue();
 			if (age == 100) age = -1;
-			System.out.println(month + " " + day + " " + hour + " " + weather + " " + rdsur + " " + type + " " + year + " " + sex + " " + age);
+			//System.out.println(month + " " + day + " " + hour + " " + weather + " " + rdsur + " " + type + " " + year + " " + sex + " " + age);
 			output = Calculations.danger(new Entry(-1, month, day, hour, weather, rdsur, type, year, sex, age, -1), 3, data);
 			
-			total.setText("All: \t" + String.format("%.2f", output.allPer()*100) + "% \t" + String.format("%.2f",output.allSev()*100) + "%");
+			total.setText("All: " + String.format("%.2f", output.allPer()*100) + "%                      " + String.format("%.2f",output.allSev()*100) + "%");
 			
 			for (int i = 0; i < 3; i++) {
-				topPer[i].setText(output.tpType(2 - i) + ": \t" + String.format("%.2f", output.tpPer(2 - i)*100) + "% \t" + String.format("%.2f", output.tpSev(2 - i)*100) + "%");
-				//tpSev[i].setText("" + output.tpSev(2 - i));
-				topSev[i].setText(output.tsType(2 - i) + ": \t" + String.format("%.2f", output.tsPer(2 - i)*100) + "% \t" + String.format("%.2f", output.tsSev(2 - i)*100) + "%");
-				//tsSev[i].setText("" + output.tsSev(2 - i));
+				topPer[i].setText(output.tpType(2 - i) + ": " + String.format("%.2f", output.tpPer(2 - i)*100) + "%                      " + String.format("%.2f", output.tpSev(2 - i)*100) + "%");
+				topSev[i].setText(output.tsType(2 - i) + ": " + String.format("%.2f", output.tsPer(2 - i)*100) + "%                      " + String.format("%.2f", output.tsSev(2 - i)*100) + "%");
 			}
 		}
 	}

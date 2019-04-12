98efe1fb767de567dfb2d77b8e9f7855bbec3aae
Chandler Co <1chandler.co@gmail.com>
2019-04-11 22:28:29 -0400

    Added Javadoc comments

--- JavaProject/src/collisionCourse/ComboBox.java
+++ JavaProject/src/collisionCourse/ComboBox.java
@@ -4,6 +4,17 @@ import javax.swing.JComboBox;
 import javax.swing.JPanel;
 
 public class ComboBox extends JComboBox{
+	/**
+	 * Creates  JComboBox, and also sets other commonly set parameters
+	 * 
+	 * @param items The options for the JComboBox
+	 * @param width The width of the element
+	 * @param height The height of the element
+	 * @param x The horizontal position from the left in pixels
+	 * @param y The vertical position from the top in pixels
+	 * @param defI The index of the default position
+	 * @param panel The JPanel to be added to
+	 */
 	public ComboBox(String[] items, int width, int height, int x, int y, int defI, JPanel panel) {
 		super(items);
 		setSize(width, height);
--- JavaProject/src/collisionCourse/Label.java
+++ JavaProject/src/collisionCourse/Label.java
@@ -4,6 +4,16 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Label extends JLabel{
+	/**
+	 * Creates a JLabel and set commonly used paramters
+	 * 
+	 * @param label The text in the JLabel
+	 * @param width The width of the label
+	 * @param height The height of the label
+	 * @param x The horizontal postion from the left
+	 * @param y The vertical position from the top
+	 * @param panel The JPanel to be added to
+	 */
 	public Label(String label, int width, int height, int x, int y, JPanel panel) {
 		super(label);
 		setSize(width, height);
